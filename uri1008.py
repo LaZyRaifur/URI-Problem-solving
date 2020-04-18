2da817a4585487f75ae491191cf77146a57cd6c9
Raifurrahim <raifurrahimcse13@gmail.com>
2020-04-18 15:43:52 +0600

    uri beginner problem

---
+++ uri1008.py
@@ -0,0 +1,6 @@
+number = int (input())
+per_hour = int (input())
+worked_hour = float(input())
+
+print("NUMBER = %d"%number)
+print("SALARY = U$ %.02f"%(per_hour*worked_hour))
---
+++ uri1009.py
@@ -0,0 +1,6 @@
+name = str(input(""))
+salary = float(input())
+total_sell = float(input())
+
+total = (.15*total_sell)+salary
+print("TOTAL = R$ %.2f"%total)
