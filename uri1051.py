f64ee429f394745f4aaabe6778dca0b7431e4a8b
Raifurrahim <raifurrahimcse13@gmail.com>
2020-07-12 19:17:50 +0600

    added more solution for uri online judge platform

--- uri1051.py
+++ uri1051.py
@@ -1,2 +1,26 @@
+res1 = 0.0
+res2 = 0.0
+res3 = 0.0
+d = 0.0
+c = 0.0
+b = 0.0
 a = float(input())
-if a >= 2000.01 and a <= 3000.00:
+
+b = a - 2000.00
+if b >= 1000.00:
+    res1 = (1000 * 8) / 100
+    c = b - 1000.00
+else:
+    res1 = (b * 8) / 100
+if c > 1500.00:
+    res2 = (1500 * 18) / 100
+    d = c - 1500
+else:
+    res2 = (c * 18) / 100
+
+res3 = (d * 28) / 100
+if a > 2000.00:
+    print("R$ %.2f" % (res1 + res2 + res3))
+
+if a >= 0.0 and a <= 2000.00:
+    print("Isento")
